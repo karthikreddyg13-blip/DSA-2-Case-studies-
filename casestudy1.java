6817050e23248350d51c64144e75071543ea5ad8
karthikreddyg13-blip <karthikreddyg13@gmail.com>
2026-06-02 13:07:08 +0530

    Implement AVL Tree with insertion and inorder traversal

---
+++ casestudy1.java
@@ -0,0 +1,101 @@
+class Node {
+    int key, height;
+    Node left, right;
+
+    Node(int d) {
+        key = d;
+        height = 1;
+    }
+}
+
+public class AVLTree {
+
+    Node root;
+
+    int height(Node N) {
+        if (N == null)
+            return 0;
+        return N.height;
+    }
+
+    int max(int a, int b) {
+        return (a > b) ? a : b;
+    }
+
+    Node rightRotate(Node y) {
+        Node x = y.left;
+        Node T2 = x.right;
+
+        x.right = y;
+        y.left = T2;
+
+        y.height = max(height(y.left), height(y.right)) + 1;
+        x.height = max(height(x.left), height(x.right)) + 1;
+
+        return x;
+    }
+
+    Node leftRotate(Node x) {
+        Node y = x.right;
+        Node T2 = y.left;
+
+        y.left = x;
+        x.right = T2;
+
+        x.height = max(height(x.left), height(x.right)) + 1;
+        y.height = max(height(y.left), height(y.right)) + 1;
+
+        return y;
+    }
+
+    int getBalance(Node N) {
+        if (N == null)
+            return 0;
+        return height(N.left) - height(N.right);
+    }
+
+    Node insert(Node node, int key) {
+
+        if (node == null)
+            return new Node(key);
+
+        if (key < node.key)
+            node.left = insert(node.left, key);
+        else if (key > node.key)
+            node.right = insert(node.right, key);
+        else
+            return node;
+
+        node.height = 1 + max(height(node.left), height(node.right));
+
+        int balance = getBalance(node);
+
+        if (balance > 1 && key < node.left.key)
+            return rightRotate(node);
+
+        if (balance < -1 && key > node.right.key)
+            return leftRotate(node);
+
+        return node;
+    }
+
+    void inorder(Node node) {
+        if (node != null) {
+            inorder(node.left);
+            System.out.print(node.key + " ");
+            inorder(node.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        AVLTree tree = new AVLTree();
+
+        tree.root = tree.insert(tree.root, 101);
+        tree.root = tree.insert(tree.root, 105);
+        tree.root = tree.insert(tree.root, 103);
+        tree.root = tree.insert(tree.root, 110);
+
+        System.out.println("Sorted Course IDs:");
+        tree.inorder(tree.root);
+    }
+}
